318f76e84be04a78ddc7e60bf6c3fac9744ca260
LuizGuimaraesz <luizgustavogs123123@gmail.com>
2026-05-12 10:46:46 -0300

    Add Zod

--- src/middlewares/error-handling.ts
+++ src/middlewares/error-handling.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 import { AppError } from "@/utils/App-error";
+import { ZodError } from "zod";
 
 export function errorHandling(
   error: Error,
@@ -11,5 +12,11 @@ export function errorHandling(
     return response.status(error.statusCode).json({ error: error.message });
   }
 
+  if (error instanceof ZodError) {
+    return response.status(400).json({
+      message: "Validation error",
+      issues: error.format(),
+    });
+  }
   response.status(500).json({ error: "Internal Server Error" });
 }
