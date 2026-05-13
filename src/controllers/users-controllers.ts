1020411d41a3a47305dcd3de133e92e2c2211aea
LuizGuimaraesz <luizgustavogs123123@gmail.com>
2026-05-13 14:44:40 -0300

    Create user

--- src/controllers/users-controllers.ts
+++ src/controllers/users-controllers.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from "express";
 import { z } from "zod";
 import { hash } from "bcrypt";
+import { prisma } from "@/database/prisma";
+import { AppError } from "@/utils/App-error";
 
 export class UserController {
   async create(request: Request, response: Response) {
@@ -14,8 +16,24 @@ export class UserController {
 
     const hashedPassword = await hash(password, 8);
 
-    return response.json({
-      message: "User created successfully",
+    const userSameEmail = await prisma.user.findFirst({
+      where: { email },
     });
+
+    if (userSameEmail) {
+      throw new AppError("User with same email already exists");
+    }
+
+    const user = await prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+      },
+    });
+
+    const { password: _, ...userWithoutPassword } = user;
+
+    return response.status(201).json(userWithoutPassword);
   }
 }
