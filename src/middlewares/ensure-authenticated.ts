48194422415597a57b6e010a5d98657290ae6321
LuizGuimaraesz <luizgustavogs123123@gmail.com>
2026-05-19 11:52:27 -0300

    Add delivery controller and routes

--- src/middlewares/ensure-authenticated.ts
+++ src/middlewares/ensure-authenticated.ts
@@ -9,7 +9,7 @@ interface TokenPayload {
   sub: string;
 }
 
-function ensureAuthenticated(
+export function ensureAuthenticated(
   request: Request,
   response: Response,
   next: NextFunction,
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { usersRoutes } from "@/routes/users-routes";
 import { sessionRoutes } from "./session-route";
+import { deliveriesRoutes } from "./deliveries-routes";
 
 export const routes = Router();
 
 routes.use("/users", usersRoutes);
 routes.use("/sessions", sessionRoutes);
+routes.use("/deliveries", deliveriesRoutes);
