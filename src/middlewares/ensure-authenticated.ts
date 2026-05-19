94b7bf7a0900bd8407f4113185aa30ce30a94754
LuizGuimaraesz <luizgustavogs123123@gmail.com>
2026-05-19 10:49:54 -0300

    Add middleware ensure-authenticated

---
+++ src/middlewares/ensure-authenticated.ts
@@ -0,0 +1,40 @@
+import { Request, Response, NextFunction } from "express";
+import { AppError } from "@/utils/App-error";
+import { authConfig } from "@/config/auth";
+import { verify } from "jsonwebtoken";
+import { throwDeprecation } from "node:process";
+
+interface TokenPayload {
+  role: string;
+  sub: string;
+}
+
+function ensureAuthenticated(
+  request: Request,
+  response: Response,
+  next: NextFunction,
+) {
+  try {
+    const authHeader = request.headers.authorization;
+
+    if (!authHeader) {
+      throw new AppError("JWT token not found", 401);
+    }
+
+    const [, token] = authHeader.split(" ");
+
+    const { role, sub: user_id } = verify(
+      token,
+      authConfig.jwt.secret,
+    ) as TokenPayload;
+
+    request.user = {
+      id: user_id,
+      role,
+    };
+
+    return next();
+  } catch (error) {
+    throw new AppError("Invalid jwt token", 401);
+  }
+}
